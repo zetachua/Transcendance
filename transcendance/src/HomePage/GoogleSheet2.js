40d891a3f788acbd4c8e4875a2879136ce062199
zetachua <zetachua246@gmail.com>
2023-04-02 16:30:13 +0800

    Update GoogleSheet2.js

--- transcendance/src/HomePage/GoogleSheet2.js
+++ transcendance/src/HomePage/GoogleSheet2.js
@@ -12,6 +12,10 @@ const HomeGallery = () => {
     
     useEffect(()=>{
       console.log("hi")
+      getPics();
+    },[]);
+
+    async function getPics(){
       fetch(FULL_URL)
       .then(res => res.text())
       .then(rep => {
@@ -19,34 +23,40 @@ const HomeGallery = () => {
           setRows(data.table.rows);
           console.log(rows);
       })
-    },[]);
-
-return (
-    <HomePage>
-      <Container>
-        <Border></Border>
-        <HomePageImage src={"https://drive.google.com/uc?export=view&id="+rows[0].c[0].v} alt="drive image"></HomePageImage>
-      </Container>
-      <a href="#" style={{margin:'auto',textDecoration:'none',color:'black'}}><h2>CREATIONS</h2></a>
-      <Container>
-        <Border></Border>
-        <HomePageImage src={"https://drive.google.com/uc?export=view&id="+rows[1].c[0].v} alt="drive image"></HomePageImage>
-      </Container>
-      <a href="#" style={{margin:'auto',textDecoration:'none',color:'black'}}><h2>GALLERY</h2></a>
-      <HomePageImage style={{width:'92%', margin:'auto'}}src={"https://drive.google.com/uc?export=view&id="+rows[4].c[0].v} alt="drive image"></HomePageImage>
-      <a href="#" style={{margin:'auto',textDecoration:'none',color:'black'}}><h2>BIOGRAPHY</h2></a>
-      <Container>
-        <Border></Border>
-        <HomePageImage src={"https://drive.google.com/uc?export=view&id="+rows[3].c[0].v} alt="drive image"></HomePageImage>
-      </Container>
-     <a href="#" style={{margin:'auto',textDecoration:'none',color:'black'}}><h2>Transcendance</h2></a>
-      <Container>
-        <Border></Border>
-        <HomePageImage src={"https://drive.google.com/uc?export=view&id="+rows[2].c[0].v} alt="drive image"></HomePageImage>
-      </Container>
-  </HomePage>
-
-  );
+    }
+    if(rows.length==0){
+      console.log("not loaded yet")
+      return(<div>Loading</div>);
+    }
+    else{
+      console.log(rows, rows.length==0);
+      return (
+        <HomePage>
+          <Container>
+            <Border></Border>
+            <HomePageImage src={"https://drive.google.com/uc?export=view&id="+rows[0].c[0].v} alt="drive image"></HomePageImage>
+          </Container>
+          <a href="#" style={{margin:'auto',textDecoration:'none',color:'black'}}><h2>CREATIONS</h2></a>
+          <Container>
+            <Border></Border>
+            <HomePageImage src={"https://drive.google.com/uc?export=view&id="+rows[1].c[0].v} alt="drive image"></HomePageImage>
+          </Container>
+          <a href="#" style={{margin:'auto',textDecoration:'none',color:'black'}}><h2>GALLERY</h2></a>
+          <HomePageImage style={{width:'92%', margin:'auto'}}src={"https://drive.google.com/uc?export=view&id="+rows[4].c[0].v} alt="drive image"></HomePageImage>
+          <a href="#" style={{margin:'auto',textDecoration:'none',color:'black'}}><h2>BIOGRAPHY</h2></a>
+          <Container>
+            <Border></Border>
+            <HomePageImage src={"https://drive.google.com/uc?export=view&id="+rows[3].c[0].v} alt="drive image"></HomePageImage>
+          </Container>
+         <a href="#" style={{margin:'auto',textDecoration:'none',color:'black'}}><h2>Transcendance</h2></a>
+          <Container>
+            <Border></Border>
+            <HomePageImage src={"https://drive.google.com/uc?export=view&id="+rows[2].c[0].v} alt="drive image"></HomePageImage>
+          </Container>
+      </HomePage>
+    
+      );
+    }
 }
 
 export default HomeGallery;
